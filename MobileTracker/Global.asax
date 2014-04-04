c37a56e97e650345bec745a4fc371661708b9dae
delicz1 <dspringer@seznam.cz>
2014-04-04 21:40:46 +0200

    Počátek práce

---
+++ MobileTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MobileTracker.MvcApplication" Language="C#" %>
---
+++ MobileTracker/Models/AccountViewModels.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MobileTracker.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Uživatelské jméno")]
+        public string UserName { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Původní heslo")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Heslo musí být {2} znaků dlouhé.", MinimumLength = 4)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nové heslo")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Nové heslo znovu")]
+        [Compare("NewPassword", ErrorMessage = "Nově zadaná hesla nejsou stejná.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Uživatelské jméno")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Heslo")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamatovat mě?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Uživatelské jméno")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Heslo musí být {2} znaků dlouhé.", MinimumLength = 4)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Heslo")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Heslo znovu")]
+        [Compare("Password", ErrorMessage = "Nově zadaná hesla nejsou stejná.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
