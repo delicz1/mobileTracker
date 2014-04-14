75783e0091212e2637435705396f073f796e0e45
delicz1 <dspringer@seznam.cz>
2014-04-15 01:54:28 +0200

    datetimepicker, prvni zobrazeni na mape

---
+++ MobileTracker/Controllers/GpsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using MobileTracker.Models;
+using MobileTracker.Models.Manager;
+
+namespace MobileTracker.Controllers
+{
+    public class GpsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [Authorize(Roles = IdentityManager.RoleUser)]
+        public ActionResult MapActualPosition(int deviceId)
+        {
+//            var timestamp = DateManager.ToUnixTimestamp(DateTime.UtcNow) - 5 * DateManager.DAY_SECOND;
+            var gpsQuery = from g in db.Gpses select g;
+            gpsQuery = gpsQuery.Where(g => g.DeviceId.Equals(deviceId));
+            gpsQuery = gpsQuery.OrderByDescending(g => g.Time);
+            return View(gpsQuery.First());
+        }
+
+        [Authorize(Roles = IdentityManager.RoleUser)]
+        public ActionResult MapDateInterval(int deviceId)
+        {
+            var gpsQuery = from g in db.Gpses select g;
+            gpsQuery = gpsQuery.Where(g => g.DeviceId.Equals(deviceId));
+            gpsQuery = gpsQuery.OrderBy(g => g.Time);
+            return View(db.EventTypes.ToList());
+        }
+    }
+}
---
+++ MobileTracker/Models/Manager/DateManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileTracker.Models.Manager
+{
+    public class DateManager
+    {
+        public const int DAY_SECOND = 86400;
+        public static long ToUnixTimestamp(DateTime target)
+        {
+            var date = new DateTime(1970, 1, 1, 0, 0, 0, target.Kind);
+            var unixTimestamp = System.Convert.ToInt64((target - date).TotalSeconds);
+
+            return unixTimestamp;
+        }
+
+        public static DateTime ToDateTime(DateTime target, long timestamp)
+        {
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, target.Kind);
+
+            return dateTime.AddSeconds(timestamp);
+        }
+    }
+}
